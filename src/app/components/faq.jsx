36c11e164c21a5bb75aacd554441b06b75c9a609
Elgara5364 <steveloardi5364@gmail.com>
2024-05-27 18:37:10 +0900

    add lib for faq list

--- src/app/components/faq.jsx
+++ src/app/components/faq.jsx
@@ -1,8 +1,29 @@
+import Image from "next/image";
+import Close from "/public/xmark-solid.svg";
+import Plus from "/public/plus-solid.svg";
+import { faqList } from "../lib/faqList";
+
 const Faq = () => {
+  // console.log(faqList);
   return (
-    <section className="h-[616px] py-[72px]  bg-black mt-2 text-center">
-      <h2 className="text-white text-5xl font-black">Tanya Jawab Umum</h2>
-      <div className="mt-6"></div>
+    <section className="h-full py-[72px]  bg-black mt-2 ">
+      <div className="max-w-[1260px] mx-auto">
+        <h2 className="text-white text-5xl font-black text-center">
+          Tanya Jawab Umum
+        </h2>
+        <div className="mt-8">
+          {faqList.map((item, index) => (
+            <div
+              className=" max-w-[1260px] bg-[#2d2d2d] p-6 flex justify-between mb-2"
+              key={index}>
+              <button className="text-2xl font-normal text-white">
+                {item.title}
+              </button>
+              <Image src={Plus} alt="Read More" width={36} height={36} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/app/lib/faqList.js
@@ -0,0 +1,26 @@
+export const faqList = [
+  {
+    title: "Apa itu Netflix?",
+    desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet.",
+  },
+  {
+    title: "Berapa biaya berlangganan Netflix?",
+    desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet.",
+  },
+  {
+    title: "Dimana saya bisa menonton?",
+    desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet.",
+  },
+  {
+    title: "Bagaimana saya bisa membatalkannya?",
+    desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet.",
+  },
+  {
+    title: "Apa yang bisa ditonton di Netflix?",
+    desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet.",
+  },
+  {
+    title: "Apakah Netflix sesuai bagi anak-anak?",
+    desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet.",
+  },
+];
